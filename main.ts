0620f3fce2e0f05c940ea858f1afb9a89d989b95
drifting4L <125625093+drifting4L@users.noreply.github.com>
2023-02-18 01:04:05 -0500

    Yeah, you know the deal

--- main.ts
+++ main.ts
@@ -1,16 +1,24 @@
-input.onButtonPressed(Button.AB, function () {
-    music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 3368, 5000, 0, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+input.onButtonPressed(Button.A, function () {
+    while (true) {
+        led.stopAnimation()
+        music.stopAllSounds()
+    }
 })
-loops.everyInterval(1000, function () {
-    music.setVolume(50)
-    music.playMelody("E D G F B A C5 B ", 150)
-    music.rest(music.beat(BeatFraction.Breve))
+input.onGesture(Gesture.Shake, function () {
+    while (true) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 3368, 5000, 0, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+    }
 })
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.No)
     basic.clearScreen()
     basic.pause(100)
-    basic.showString("Hi!")
+    basic.showString("Alert!")
     basic.clearScreen()
     basic.pause(200)
+})
+loops.everyInterval(200, function () {
+    music.setVolume(50)
+    music.playMelody("E D G F B A C5 B ", 110)
+    music.rest(music.beat(BeatFraction.Breve))
 })
