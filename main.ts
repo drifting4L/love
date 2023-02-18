c88855af95ea259531b7ee9c1d803c8356018013
drifting4L <125625093+drifting4L@users.noreply.github.com>
2023-02-18 01:15:52 -0500

    V8 Final Edition?

--- main.ts
+++ main.ts
@@ -4,21 +4,21 @@ input.onButtonPressed(Button.A, function () {
         music.stopAllSounds()
     }
 })
-input.onGesture(Gesture.Shake, function () {
+input.onGesture(Gesture.TiltLeft, function () {
     while (true) {
-        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 3368, 5000, 0, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+        basic.showIcon(IconNames.No)
+        basic.clearScreen()
+        basic.pause(100)
+        basic.showString("Alert!")
+        basic.clearScreen()
+        basic.pause(200)
     }
 })
-basic.forever(function () {
-    basic.showIcon(IconNames.No)
-    basic.clearScreen()
-    basic.pause(100)
-    basic.showString("Alert!")
-    basic.clearScreen()
-    basic.pause(200)
-})
-loops.everyInterval(200, function () {
+input.onGesture(Gesture.ScreenUp, function () {
     music.setVolume(50)
-    music.playMelody("E D G F B A C5 B ", 110)
-    music.rest(music.beat(BeatFraction.Breve))
+    while (true) {
+        music.playMelody("E D G F B A C5 B ", 110)
+        music.rest(music.beat(BeatFraction.Whole))
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 3368, 5000, 0, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+    }
 })
