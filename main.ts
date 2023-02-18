b749fde1de478b5a96bee22a6d63ce910cdbbcbf
drifting4L <125625093+drifting4L@users.noreply.github.com>
2023-02-18 00:28:33 -0500

    Just added more time and shortened the words.

--- main.ts
+++ main.ts
@@ -1,8 +1,16 @@
+input.onButtonPressed(Button.AB, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 3368, 5000, 0, 255, 9999, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.InBackground)
+})
+loops.everyInterval(1000, function () {
+    music.setVolume(50)
+    music.playMelody("E D G F B A C5 B ", 150)
+    music.rest(music.beat(BeatFraction.Breve))
+})
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
     basic.clearScreen()
-    basic.pause(500)
-    basic.showString("Hello!")
+    basic.pause(100)
+    basic.showString("Hi!")
     basic.clearScreen()
-    basic.pause(5000)
+    basic.pause(200)
 })
