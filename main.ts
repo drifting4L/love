47f9d9215630dbfb272ad22d1d4ab2f8e7e53766
drifting4L <125625093+drifting4L@users.noreply.github.com>
2023-02-17 23:31:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.clearScreen()
+    basic.pause(2000)
+    basic.showString("Hello!")
+    basic.clearScreen()
+    basic.pause(5000)
+})
